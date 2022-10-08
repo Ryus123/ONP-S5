264930267bdd23806fe04c273175c5c18e690625
Ryus123 <delaremmarius@gmail.com>
2022-10-08 14:55:26 +0200

    TD3 Ex2 fait sauf 8 et 9

--- TD3_DELAR_Emmarius.py
+++ TD3_DELAR_Emmarius.py
@@ -1,2 +1,41 @@
+from unicodedata import decimal
+import numpy as np
 import pandas as pd
 import os
+from pprint import pprint
+"""-----------------------------------IMPORT----------------------------------------"""
+
+###Exercice 1 : Définir le plus simplement possible v1, v2, v3, v4
+v1 = np.array([5,9,1,3])
+v2 = 2*np.ones(20)
+v3 = np.arange(1,17)
+v4 = np.arange(28,11, -2)
+
+###Exercice 2
+#3. Charger ce fichier en utilisant les fonctionnalités de pandas.
+chemin1 = os.getcwd()+"/DataTD/Enquete_prog.csv"
+chemin2 = os.getcwd()+"/Data/Enquete_prog.csv"
+
+try:
+    Donnees = pd.read_csv(chemin1, delimiter=";")
+except:
+    Donnees = pd.read_csv(chemin2, delimiter=";")
+
+df = pd.DataFrame(Donnees)
+
+#4. A l'aide de la fonction adaptée, afficher le nom de chaque colonne
+#print(df.columns)
+
+#5. Décrire les données (nombre d’observations, nombre de variables)
+#print("Nombre d'observation : {}, Nombre de variable : {}".format(len(df.axes[0]), len(df.axes[1])))
+
+#6. Combien y a-t-il de données manquantes pour la variable MaritalStatus ?
+#print(df["MaritalStatus"].isna().sum())
+
+#7. Compléter pour la variable Age
+#print(np.round(df["Age"].describe(), decimals=2))
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import numpy as np
-a = np.array([[-3, 5, 6],[-1, 2, 2], [1, -1, -1]])
-
-
-print(np.append(a[0], 1))
+a = 2*np.ones(300)
+a[3]= np.nan
+print(a)
